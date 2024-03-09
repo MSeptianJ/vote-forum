e25ea7a07877a346aaca0d6050513a8973626729
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-03-09 07:08:36 +0700

    First Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Votrum
+
+This is a project for a forum about Dicoding Academy. this repo is for my submission in dicoding Class "Menjadi React Web Developer Expert"
+
+There are two submission in this repoc, you can see them in their branch.
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className=" h-screen w-full bg-gray-600">
+      <h1 className=" m-auto text-center text-xl font-bold uppercase text-white">
+        Hello
+      </h1>
+    </div>
+  );
+}
+
+export default App;

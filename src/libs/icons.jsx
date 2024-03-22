f465b867fa74433bbf858cf0db6bd55ed06cdba9
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-03-22 10:51:06 +0700

    Organize Icons

---
+++ src/libs/icons.jsx
@@ -0,0 +1,27 @@
+import {
+  MdHome,
+  MdLeaderboard,
+  MdOutlineHome,
+  MdOutlineLeaderboard,
+  MdSearch,
+} from "react-icons/md";
+
+export const IconHome = () => {
+  return <MdHome />;
+};
+
+export const IconHomeOutline = () => {
+  return <MdOutlineHome />;
+};
+
+export const IconLeaderboard = () => {
+  return <MdLeaderboard />;
+};
+
+export const IconLeaderboardOutline = () => {
+  return <MdOutlineLeaderboard />;
+};
+
+export const IconSearch = () => {
+  return <MdSearch />;
+};

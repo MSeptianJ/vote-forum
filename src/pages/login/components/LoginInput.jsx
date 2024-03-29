2f55ac36516ec2386642710afb544ca6237775df
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-03-29 20:28:03 +0700

    Add Login Page

---
+++ src/pages/login/components/LoginInput.jsx
@@ -0,0 +1,41 @@
+import PropTypes from "prop-types";
+
+const LoginInput = () => {
+  return (
+    <form className="mt-3 flex w-full flex-col gap-4">
+      <div className="grid w-full space-y-1">
+        <label htmlFor="emailLogin" className="w-full text-sm text-gray-300">
+          Email
+        </label>
+        <input
+          type="text"
+          id="emailLogin"
+          className="w-full rounded-[4px] border border-secondary bg-transparent"
+        />
+      </div>
+
+      <div className="grid w-full space-y-1">
+        <label htmlFor="passLogin" className="w-full text-sm text-gray-300">
+          Password
+        </label>
+        <input
+          type="text"
+          id="passLogin"
+          className="w-full rounded-[4px] border border-secondary bg-transparent"
+        />
+      </div>
+
+      <div className="mt-3 flex w-full justify-center">
+        <input
+          type="submit"
+          value="Submit"
+          className=" w-1/2 max-w-52 rounded-md border border-secondary bg-secondary py-1 text-sm font-bold hover:bg-primary"
+        />
+      </div>
+    </form>
+  );
+};
+
+LoginInput.propTypes = {};
+
+export default LoginInput;
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,30 @@
-import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import LoginInput from "./components/LoginInput";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="mt-8 w-full px-5">
+      <div className="mx-auto mb-5 flex w-full max-w-xl flex-col gap-2 rounded-md bg-primary p-5 text-white shadow-lg">
+        <h4 className=" text-xl font-bold">New Thread</h4>
+
+        <div className=" w-full text-sm text-gray-300">
+          <p>
+            Anda harus melakukan login untuk membuat{" "}
+            <span className=" font-bold text-white">Thread</span>
+          </p>
+        </div>
+
+        <LoginInput />
+
+        <div className=" mt-2 flex w-full gap-1 text-xs text-gray-300">
+          <p>Tidak memiliki akun?</p>
+          <Link to={"/register"} className=" text-accent hover:underline">
+            Register disini
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 LoginPage.propTypes = {};

7d98f9cfd7ad95433e0581c8ca75fe08ed2397ab
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-03-31 12:14:52 +0700

    Lint Thread Page

--- src/pages/thread/index.jsx
+++ src/pages/thread/index.jsx
@@ -1,53 +1,54 @@
-import CommentList from "../../components/Comment/CommentList";
-import ThreadWithComment from "../../components/Thread/ThreadWithComment";
+import React from 'react';
+import CommentList from '../../components/Comment/CommentList';
+import ThreadWithComment from '../../components/Thread/ThreadWithComment';
 
-const ThreadPage = () => {
+function ThreadPage() {
   const data = {
-    id: "thread-1",
-    title: "Thread Pertama",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptatibus, maxime excepturi eum autem at veritatis repellat commodi eligendi nostrum, laboriosam iure pariatur doloremque nihil adipisci libero cupiditate natus assumenda. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae aliquid inventore eligendi natus. Vero cupiditate ut culpa commodi facilis excepturi sunt, a iure iste quas consequuntur omnis recusandae veniam assumenda. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias cum suscipit, quis eaque fuga autem, nisi mollitia aperiam neque impedit veniam minus dolorum recusandae officiis repellat error nihil saepe sunt.",
-    category: "General",
-    createdAt: "2021-06-21T07:00:00.000Z",
+    id: 'thread-1',
+    title: 'Thread Pertama',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptatibus, maxime excepturi eum autem at veritatis repellat commodi eligendi nostrum, laboriosam iure pariatur doloremque nihil adipisci libero cupiditate natus assumenda. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae aliquid inventore eligendi natus. Vero cupiditate ut culpa commodi facilis excepturi sunt, a iure iste quas consequuntur omnis recusandae veniam assumenda. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias cum suscipit, quis eaque fuga autem, nisi mollitia aperiam neque impedit veniam minus dolorum recusandae officiis repellat error nihil saepe sunt.',
+    category: 'General',
+    createdAt: '2021-06-21T07:00:00.000Z',
     owner: {
-      id: "users-1",
-      name: "John Doe",
-      avatar: "https://generated-image-url.jpg",
+      id: 'users-1',
+      name: 'John Doe',
+      avatar: 'https://generated-image-url.jpg',
     },
     upVotesBy: [],
     downVotesBy: [],
     comments: [
       {
-        id: "comment-1",
-        content: "Ini adalah komentar pertama",
-        createdAt: "2021-06-21T07:00:00.000Z",
+        id: 'comment-1',
+        content: 'Ini adalah komentar pertama',
+        createdAt: '2021-06-21T07:00:00.000Z',
         owner: {
-          id: "users-1",
-          name: "John Doe",
-          avatar: "https://generated-image-url.jpg",
+          id: 'users-1',
+          name: 'John Doe',
+          avatar: 'https://generated-image-url.jpg',
         },
         upVotesBy: [],
         downVotesBy: [],
       },
       {
-        id: "comment-1",
-        content: "Ini adalah komentar pertama",
-        createdAt: "2021-06-21T07:00:00.000Z",
+        id: 'comment-2',
+        content: 'Ini adalah komentar pertama',
+        createdAt: '2021-06-21T07:00:00.000Z',
         owner: {
-          id: "users-1",
-          name: "John Doe",
-          avatar: "https://generated-image-url.jpg",
+          id: 'users-1',
+          name: 'John Doe',
+          avatar: 'https://generated-image-url.jpg',
         },
         upVotesBy: [],
         downVotesBy: [],
       },
       {
-        id: "comment-1",
-        content: "Ini adalah komentar pertama",
-        createdAt: "2021-06-21T07:00:00.000Z",
+        id: 'comment-3',
+        content: 'Ini adalah komentar pertama',
+        createdAt: '2021-06-21T07:00:00.000Z',
         owner: {
-          id: "users-1",
-          name: "John Doe",
-          avatar: "https://generated-image-url.jpg",
+          id: 'users-1',
+          name: 'John Doe',
+          avatar: 'https://generated-image-url.jpg',
         },
         upVotesBy: [],
         downVotesBy: [],
@@ -55,13 +56,34 @@ const ThreadPage = () => {
     ],
   };
 
+  const onUpVote = () => {
+    console.log('upvote');
+  };
+
+  const onDownVote = () => {
+    console.log('downvote');
+  };
+
+  const onComment = () => {
+    console.log('Comments');
+  };
+
   return (
     <div className="mb-5 mt-8 w-full px-5">
-      <ThreadWithComment data={data} />
-      <CommentList commentData={data.comments} />
+      <ThreadWithComment
+        data={data}
+        onUpVote={onUpVote}
+        onDownVote={onDownVote}
+        onComment={onComment}
+      />
+      <CommentList
+        commentData={data.comments}
+        onUpVote={onUpVote}
+        onDownVote={onDownVote}
+      />
     </div>
   );
-};
+}
 
 ThreadPage.propTypes = {};
 

016073f2f3f36ecc11b2b4f7bd2b8bf8e9301491
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-03-31 11:58:14 +0700

    Lint Home Page

--- src/pages/home/components/HomeTopBar.jsx
+++ src/pages/home/components/HomeTopBar.jsx
@@ -1,51 +1,44 @@
-import { Link } from "react-router-dom";
-import BtnPrimary from "../../../components/BtnPrimary";
-import { IconPlus } from "../../../libs/icons";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import BtnPrimary from '../../../components/BtnPrimary';
+import { IconPlus } from '../../../libs/icons';
 
-const HomeTopBar = () => {
+function HomeTopBar() {
   return (
     <div className="md flex w-full items-center justify-center gap-5 px-5 text-white">
       <div className=" m-auto hidden w-10/12 rounded-md bg-primary p-4 text-white shadow-lg md:block">
         <h3 className="mb-2 text-xl font-bold">Categories</h3>
         <div className=" flex gap-3 overflow-x-auto">
           <BtnPrimary
-            btnText={"#dicoding"}
-            btnStyles={
-              "text-center text-sm h-full border border-secondary p-1 px-3 rounded-md hover:bg-secondary"
-            }
-            btnFunction={() => console.log("boop")}
+            btnText="#dicoding"
+            btnStyles="text-center text-sm h-full border border-secondary p-1 px-3 rounded-md hover:bg-secondary"
+            btnFunction={() => console.log('boop')}
           />
           <BtnPrimary
-            btnText={"#Mom"}
-            btnStyles={
-              "text-center text-sm h-full border border-secondary p-1 px-3 rounded-md hover:bg-secondary"
-            }
-            btnFunction={() => console.log("boop")}
+            btnText="#Mom"
+            btnStyles="text-center text-sm h-full border border-secondary p-1 px-3 rounded-md hover:bg-secondary"
+            btnFunction={() => console.log('boop')}
           />
           <BtnPrimary
-            btnText={"#hai"}
-            btnStyles={
-              "text-center text-sm h-full border border-secondary p-1 px-3 rounded-md hover:bg-secondary"
-            }
-            btnFunction={() => console.log("boop")}
+            btnText="#hai"
+            btnStyles="text-center text-sm h-full border border-secondary p-1 px-3 rounded-md hover:bg-secondary"
+            btnFunction={() => console.log('boop')}
           />
         </div>
       </div>
 
       <div className=" w-full flex-1 rounded-md bg-secondary p-4 shadow-lg transition-colors duration-300 hover:bg-primary md:w-auto">
-        <Link to={"/submit"}>
+        <Link to="/submit">
           <BtnPrimary
-            btnStyles={
-              "w-full md:mb-1 md:mt-1 flex text-xl justify-center items-center gap-3"
-            }
-            btnText={"New Thread"}
+            btnStyles="w-full md:mb-1 md:mt-1 flex text-xl justify-center items-center gap-3"
+            btnText="New Thread"
             btnIcon={<IconPlus />}
           />
         </Link>
       </div>
     </div>
   );
-};
+}
 
 HomeTopBar.propTypes = {};
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,39 +1,48 @@
-import ThreadList from "../../components/Thread/ThreadList";
-import HomeTopBar from "./components/HomeTopBar";
+import React from 'react';
+import ThreadList from '../../components/Thread/ThreadList';
+import HomeTopBar from './components/HomeTopBar';
 
-const HomePage = () => {
+function HomePage() {
   const ThreadDocs = [
     {
-      id: "thread-1",
-      title: "Thread Pertama",
-      body: "Ini adalah thread pertama",
-      category: "General",
-      createdAt: "2021-06-21T07:00:00.000Z",
-      ownerId: "users-1",
+      id: 'thread-1',
+      title: 'Thread Pertama',
+      body: 'Ini adalah thread pertama',
+      category: 'General',
+      createdAt: '2021-06-21T07:00:00.000Z',
+      ownerId: 'users-1',
       upVotesBy: [],
       downVotesBy: [],
       totalComments: 0,
     },
     {
-      id: "thread-2",
-      title: "Thread Kedua",
-      body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi quisquam quasi nulla? Nesciunt amet ipsa atque! Hic error ex mollitia illum odit, provident maiores quod reprehenderit, velit aspernatur magni iusto!, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi quisquam quasi nulla? Nesciunt amet ipsa atque! Hic error ex mollitia illum odit, provident maiores quod reprehenderit, velit aspernatur magni iusto!,Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi quisquam quasi nulla? Nesciunt amet ipsa atque! Hic error ex mollitia illum odit, provident maiores quod reprehenderit, velit aspernatur magni iusto!",
-      category: "General",
-      createdAt: "2021-06-21T07:00:00.000Z",
-      ownerId: "users-2",
+      id: 'thread-2',
+      title: 'Thread Kedua',
+      body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi quisquam quasi nulla? Nesciunt amet ipsa atque! Hic error ex mollitia illum odit, provident maiores quod reprehenderit, velit aspernatur magni iusto!, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi quisquam quasi nulla? Nesciunt amet ipsa atque! Hic error ex mollitia illum odit, provident maiores quod reprehenderit, velit aspernatur magni iusto!,Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi quisquam quasi nulla? Nesciunt amet ipsa atque! Hic error ex mollitia illum odit, provident maiores quod reprehenderit, velit aspernatur magni iusto!',
+      category: 'General',
+      createdAt: '2021-06-21T07:00:00.000Z',
+      ownerId: 'users-2',
       upVotesBy: [],
       downVotesBy: [],
       totalComments: 0,
     },
   ];
 
+  const onUpVote = () => {
+    console.log('upvote');
+  };
+
+  const onDownVote = () => {
+    console.log('downvote');
+  };
+
   return (
     <div className="mt-8 w-full">
       <HomeTopBar />
-      <ThreadList threadData={ThreadDocs} />
+      <ThreadList threadData={ThreadDocs} onUpVote={onUpVote} onDownVote={onDownVote} />
     </div>
   );
-};
+}
 
 HomePage.propTypes = {};
 

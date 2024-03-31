343b7592769b3dc92fcdc61ca49c47f7d436e773
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-03-31 11:57:39 +0700

    Lint Error Page

--- src/pages/error/index.jsx
+++ src/pages/error/index.jsx
@@ -1,13 +1,14 @@
-import { Link, useRouteError } from "react-router-dom";
+import React from 'react';
+import { Link, useRouteError } from 'react-router-dom';
 
-const ErrorPage = () => {
+function ErrorPage() {
   const errorData = useRouteError();
   return (
     <div className=" grid min-h-screen w-full items-center text-center">
       <div className=" m-auto grid w-full gap-5">
         <div>
           <h3 className=" text-lg font-bold text-red-600">
-            {errorData?.status || "Error"}
+            {errorData?.status || 'Error'}
           </h3>
           {errorData?.status === 404 ? (
             <p>Page Not Found</p>
@@ -17,14 +18,14 @@ const ErrorPage = () => {
         </div>
 
         <div className=" m-auto w-1/3 cursor-pointer bg-gray-500 text-white transition-all duration-200 hover:bg-opacity-70">
-          <Link to={"/"} className=" block h-full w-full p-2">
+          <Link to="/" className=" block h-full w-full p-2">
             Menu
           </Link>
         </div>
       </div>
     </div>
   );
-};
+}
 
 ErrorPage.propTypes = {};
 

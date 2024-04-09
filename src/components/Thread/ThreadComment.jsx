edd5112cf01eaf244c0a027a7093f8d12e566def
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-04-09 12:14:00 +0700

    Controlled Comment Input Component

--- src/components/Thread/ThreadComment.jsx
+++ src/components/Thread/ThreadComment.jsx
@@ -1,19 +1,28 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import BtnPrimary from '../BtnPrimary';
 
 function ThreadComment({ onComment }) {
+  const [content, setContent] = useState('');
+
+  const contentHandler = (e) => {
+    setContent(e.target.innerHTML);
+  };
+
   const handleComment = () => {
-    onComment();
+    onComment(content);
   };
 
   return (
     <div className=" mt-6 flex flex-col text-white">
       <h4 className=" font-bold">Make a Comment</h4>
+
       <div
-        className=" mt-4 min-h-20 rounded-md border border-secondary p-3"
+        onInput={contentHandler}
         contentEditable
+        className=" mt-4 min-h-20 rounded-md border border-secondary p-3"
       />
+
       <div className="flex justify-end">
         <BtnPrimary
           btnText="Reply"

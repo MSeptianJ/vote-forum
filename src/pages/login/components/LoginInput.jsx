56ddb3622ff38895cddb5d7749e75b7b614fca73
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-04-19 17:13:35 +0700

    Add LoginInput Component Test

--- src/pages/login/components/LoginInput.jsx
+++ src/pages/login/components/LoginInput.jsx
@@ -20,6 +20,7 @@ function LoginInput({ onLogin }) {
         <input
           type="text"
           id="emailLogin"
+          title="Login Email"
           value={email}
           onChange={onEmailChange}
           className="w-full rounded-[4px] border border-secondary bg-transparent"
@@ -33,6 +34,7 @@ function LoginInput({ onLogin }) {
         <input
           type="password"
           id="passLogin"
+          title="Login Password"
           value={password}
           onChange={onPassChange}
           className="w-full rounded-[4px] border border-secondary bg-transparent"
